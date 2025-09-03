e17c83df52024bc34d75180f848eac6aa460fe5f
Himanxu1 <technophilic000@gmail.com>
2025-09-03 22:44:08 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "./context/AuthContext";
+import { Analytics } from "@vercel/analytics/next";
 
 import { MotionWrapper } from "./components/MotionWrapper";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Analytics />
       <AuthProvider>
         <html lang="en">
           <body
